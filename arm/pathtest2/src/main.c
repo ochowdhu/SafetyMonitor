a6cb1b05e134148d05f1bd120d65a99f146a0f48
Aaron Kane <akane@cmu.edu>
2014-08-25 19:00:10 -0400

    adding CAN and some proof writeup stuff

--- arm/pathtest2/src/main.c
+++ arm/pathtest2/src/main.c
@@ -19,6 +19,34 @@
 #include "monconfig.h"
 
 
+///////////////// CANDEF
+#define USE_CAN1
+//#define USE_CAN2 
+
+#ifdef  USE_CAN1
+  #define CANx                       CAN1
+  #define CAN_CLK                    RCC_APB1Periph_CAN1
+  #define CAN_RX_PIN                 GPIO_Pin_0
+  #define CAN_TX_PIN                 GPIO_Pin_1
+  #define CAN_GPIO_PORT              GPIOD
+  #define CAN_GPIO_CLK               RCC_AHB1Periph_GPIOD
+  #define CAN_AF_PORT                GPIO_AF_CAN1
+  #define CAN_RX_SOURCE              GPIO_PinSource0
+  #define CAN_TX_SOURCE              GPIO_PinSource1       
+#else /*USE_CAN2*/
+  #define CANx                       CAN2
+  #define CAN_CLK                    (RCC_APB1Periph_CAN1 | RCC_APB1Periph_CAN2)
+  #define CAN_RX_PIN                 GPIO_Pin_5
+  #define CAN_TX_PIN                 GPIO_Pin_13
+  #define CAN_GPIO_PORT              GPIOB
+  #define CAN_GPIO_CLK               RCC_AHB1Periph_GPIOB
+  #define CAN_AF_PORT                GPIO_AF_CAN2
+  #define CAN_RX_SOURCE              GPIO_PinSource5
+  #define CAN_TX_SOURCE              GPIO_PinSource13    
+#endif  /* USE_CAN1 */
+///////////////////////////
+
+
 // 30s minutes of data
 #define TDATASIZE	1200
 
@@ -30,7 +58,7 @@ int sim = TRUE;
 // formulas
 // struct	-- list of lists
 // taulist	-- resbuf of (step,time) pairs
-int delay;
+int delay;	
 int cstep;	// current step count
 int lcount;
 
@@ -46,6 +74,73 @@ void fillData() {
 		if ((i % 30) == 0)
 			testdata[i] |= MASK_c;
 	}
+}
+
+
+
+void InitCAN() {
+  GPIO_InitTypeDef  GPIO_InitStructure;
+  CAN_InitTypeDef        CAN_InitStructure;
+	CAN_FilterInitTypeDef  CAN_FilterInitStructure;
+	CanTxMsg TxMessage;
+  /* CAN GPIOs configuration **************************************************/
+
+  /* Enable GPIO clock */
+  RCC_AHB1PeriphClockCmd(CAN_GPIO_CLK, ENABLE);
+
+  /* Connect CAN pins to AF9 */
+  GPIO_PinAFConfig(CAN_GPIO_PORT, CAN_RX_SOURCE, CAN_AF_PORT);
+  GPIO_PinAFConfig(CAN_GPIO_PORT, CAN_TX_SOURCE, CAN_AF_PORT); 
+  
+  /* Configure CAN RX and TX pins */
+  GPIO_InitStructure.GPIO_Pin = CAN_RX_PIN | CAN_TX_PIN;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_UP;
+  GPIO_Init(CAN_GPIO_PORT, &GPIO_InitStructure);
+
+  /* CAN configuration ********************************************************/  
+  /* Enable CAN clock */
+  RCC_APB1PeriphClockCmd(CAN_CLK, ENABLE);
+  
+  /* CAN register init */
+  CAN_DeInit(CANx);
+
+  /* CAN cell init */
+  CAN_StructInit(&CAN_InitStructure);
+	// Set stuff that isn't default
+  /* CAN Baudrate = 1 MBps (CAN clocked at 30 MHz) */
+  CAN_InitStructure.CAN_BS1 = CAN_BS1_6tq;
+  CAN_InitStructure.CAN_BS2 = CAN_BS2_8tq;
+  CAN_InitStructure.CAN_Prescaler = 2;
+  CAN_Init(CANx, &CAN_InitStructure);
+
+  /* CAN filter init */
+#ifdef  USE_CAN1
+  CAN_FilterInitStructure.CAN_FilterNumber = 0;
+#else /* USE_CAN2 */
+  CAN_FilterInitStructure.CAN_FilterNumber = 14;
+#endif  /* USE_CAN1 */
+  CAN_FilterInitStructure.CAN_FilterMode = CAN_FilterMode_IdMask;
+  CAN_FilterInitStructure.CAN_FilterScale = CAN_FilterScale_32bit;
+  CAN_FilterInitStructure.CAN_FilterIdHigh = 0x0000;
+  CAN_FilterInitStructure.CAN_FilterIdLow = 0x0000;
+  CAN_FilterInitStructure.CAN_FilterMaskIdHigh = 0x0000;
+  CAN_FilterInitStructure.CAN_FilterMaskIdLow = 0x0000;
+  CAN_FilterInitStructure.CAN_FilterFIFOAssignment = 0;
+  CAN_FilterInitStructure.CAN_FilterActivation = ENABLE;
+  CAN_FilterInit(&CAN_FilterInitStructure);
+  
+  /* Transmit Structure preparation */
+  TxMessage.StdId = 0x321;
+  TxMessage.ExtId = 0x01;
+  TxMessage.RTR = CAN_RTR_DATA;
+  TxMessage.IDE = CAN_ID_STD;
+  TxMessage.DLC = 1;
+  
+  /* Enable FIFO 0 message pending Interrupt */
+  CAN_ITConfig(CANx, CAN_IT_FMP0, ENABLE);
 }
 
 void fillData2() {
@@ -93,6 +188,7 @@ void InitializeTimer()
     TIM_TimeBaseInit(TIM3, &timerInitStructure);
     TIM_Cmd(TIM3, ENABLE);
 		TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
+		
 	
 }
 
@@ -112,7 +208,19 @@ void EnableTimerInterrupt()
     nvicStructure.NVIC_IRQChannelSubPriority = 1;
     nvicStructure.NVIC_IRQChannelCmd = ENABLE;
     NVIC_Init(&nvicStructure);
+	
+	// Enable CAN
+	#ifdef  USE_CAN1 
+	nvicStructure.NVIC_IRQChannel = CAN1_RX0_IRQn;
+	#else  /* USE_CAN2 */
+	nvicStructure.NVIC_IRQChannel = CAN2_RX0_IRQn;
+	#endif /* USE_CAN1 */
+  nvicStructure.NVIC_IRQChannelPreemptionPriority = 0x0;
+  nvicStructure.NVIC_IRQChannelSubPriority = 0x0;
+  nvicStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&nvicStructure);
 }
+
 
 //extern "C" 
 void TIM2_IRQHandler()
@@ -137,7 +245,15 @@ void TIM3_IRQHandler()
         TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
     }
 }
-
+//extern "C"
+void CAN1_RX0_IRQHandler() {
+	// might not need to check flags, need to look around a bit more
+	/*if (CAN_GetITStatus(CANx, CAN_IT_FMP0) == SET) {
+		
+	} else if (CAN_GetITStatus(CANx, CAN_IT_FMP1) == SET) {
+		
+	}*/
+}
 
 int main() {
 	// local variables
